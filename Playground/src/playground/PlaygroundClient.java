7217a844b8d1fe03feee46d841cad2289484e9c2
Joshua Steward <joshua.steward@students.fhu.edu>
2017-10-30 10:00:11 -0500

    Created Project

---
+++ Playground/src/playground/PlaygroundClient.java
@@ -0,0 +1,18 @@
+
+package playground;
+
+import static java.lang.System.out;
+
+/**
+ *
+ * @author Joshua Steward
+ */
+public class PlaygroundClient {
+    
+    public static void main(String[] args) {
+        out.println("This application is intended as a code playground for Java");
+        out.println("Please input a number to make a selection:");
+        
+    }
+    
+}
